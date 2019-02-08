244eeb20ddfad70882fd51e78e22dabc246577a3
Ivan Alonso <ivan@alonso.tv>
2019-02-08 09:44:39 -0800

    updated main entry

--- src/main/java/co/alonsos/heroku/Main.java
+++ src/main/java/co/alonsos/heroku/Main.java
@@ -1,20 +1,44 @@
 package co.alonsos.heroku;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.preventers.AppContextLeakPreventer;
+import org.eclipse.jetty.util.preventers.GCThreadLeakPreventer;
+import org.eclipse.jetty.util.preventers.Java2DLeakPreventer;
+import org.eclipse.jetty.util.preventers.SecurityProviderLeakPreventer;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// The port that we should run on can be set into an environment variable
-		// Look for that variable and default to 8080 if it isn't there.
-		String webPort = System.getenv("PORT");
-		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8080";
-		}
+	static Server server;
+	static WebAppContext root;
+	static Logger log = Logger.getLogger(Main.class);
 
-		final Server server = new Server(Integer.valueOf(webPort));
-		final WebAppContext root = new WebAppContext();
+	public static void main(String[] args) {
+		try {
+			// The port that we should run on can be set into an environment variable
+			// Look for that variable and default to 8080 if it isn't there.
+			String webPort = System.getenv("PORT");
+			if (webPort == null || webPort.isEmpty()) {
+				webPort = "8080";
+			}
+			startServer(webPort);
+			log.debug("Server has started and joined, using port: " + webPort);
+		}catch (Exception e) {
+			log.error(e.getMessage(), e);
+			stopServer();
+		}
+	}
+
+	public static void startServer(String webPort) throws Exception {
+
+		server = new Server(Integer.valueOf(webPort));
+		server.addBean(new AppContextLeakPreventer());
+		server.addBean(new GCThreadLeakPreventer());
+		server.addBean(new SecurityProviderLeakPreventer());
+		server.addBean(new Java2DLeakPreventer());
+
+		root = new WebAppContext();
 
 		root.setContextPath("/");
 		// Parent loader priority is a class loader setting that Jetty accepts.
@@ -28,9 +52,28 @@ public class Main {
 		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
 		root.setResourceBase(webappDirLocation);
 
-		server.setHandler(root);
+		// NCSARequestLog requestLog = new
+		// NCSARequestLog("/Users/ivanalonso/Desktop/jetty/jetty-yyyy_mm_dd.request.log");
+		// requestLog.setAppend(true);
+		// requestLog.setExtended(false);
+		// requestLog.setLogTimeZone("GMT");
+		// requestLog.setLogLatency(true);
+		// requestLog.setRetainDays(90);
+		//
+		// server.setRequestLog(requestLog);
 
+		server.setHandler(root);
 		server.start();
 		server.join();
 	}
+
+	public static void stopServer() {
+		try {
+			server.stop();
+			server.destroy();
+		}catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
 }
+
